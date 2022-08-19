eaa82b052483e78cb1fc0870534ece53fc262cdf
Yaksat <aubekerov@list.ru>
2022-08-19 09:25:22 +0400

    add AdminLte

--- routes/web.php
+++ routes/web.php
@@ -17,5 +17,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', \App\Http\Controllers\Main\IndexController::class);
 
+Route::group(['prefix' => 'admin'], function () {
+    Route::get('/', \App\Http\Controllers\Admin\Main\IndexController::class);
+});
 
 Auth::routes();
