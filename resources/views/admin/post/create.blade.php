ae810cf03ff20ff94266fe32c6df2a4e96659997
Yaksat <aubekerov@list.ru>
2022-08-24 11:25:12 +0400

    добавил плагин select2 из AdminLTE, форич для мультиселекта, добавляем реквест и в контроллере привязку тэгов

--- resources/views/admin/post/create.blade.php
+++ resources/views/admin/post/create.blade.php
@@ -84,6 +84,16 @@
                                 @endforeach
                             </select>
                         </div>
+                        <div class="form-group">
+                            <label>Тэги</label>
+                            <select name="tag_ids[]" class="select2" multiple="multiple" data-placeholder="Выберите тэги" style="width: 100%;">
+                                @foreach($tags as $tag)
+                                    <option {{ is_array(old('tag_ids')) && in_array($tag->id, old('tag_ids')) ? ' selected' : '' }} value="{{ $tag->id }}">
+                                        {{ $tag->title }}
+                                    </option>
+                                @endforeach
+                            </select>
+                        </div>
 
                         <div class="form-group">
                             <input type="submit" class="btn btn-primary" value="Добавить">
