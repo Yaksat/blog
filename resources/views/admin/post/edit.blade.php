542948c78cbe9e99010c6f5d3567714d7ee22b79
Yaksat <aubekerov@list.ru>
2022-08-25 13:19:39 +0400

    апдейт у постов, форма редактирования, отображение изображений

--- resources/views/admin/post/edit.blade.php
+++ resources/views/admin/post/edit.blade.php
@@ -26,7 +26,7 @@
             <!-- Small boxes (Stat box) -->
             <div class="row">
                 <div class="col-12">
-                    <form action="{{ route('admin.post.update', $post->id) }}" method="POST">
+                    <form action="{{ route('admin.post.update', $post->id) }}" method="POST" enctype="multipart/form-data">
                         @csrf
                         @method('patch')
                         <div class="form-group w-25">
@@ -47,8 +47,8 @@
                         </div>
                         <div class="form-group w-50">
                             <label for="exampleInputFile">Добавить превью</label>
-                            <div class="w-25">
-                                <img src="{{ $post->preview_image }}" alt="preview_image" class="w-50">
+                            <div class="w-25 mb-2">
+                                <img src="{{ url('storage/' . $post->preview_image) }}" alt="preview_image" class="w-50">
                             </div>
                             <div class="input-group">
                                 <div class="custom-file">
@@ -65,8 +65,8 @@
                         </div>
                         <div class="form-group w-50">
                             <label for="exampleInputFile">Добавить главное изображение</label>
-                            <div class="w-25">
-                                <img src="{{ $post->main_image }}" alt="main_image" class="w-50">
+                            <div class="w-50 mb-2">
+                                <img src="{{ url('storage/' . $post->main_image) }}" alt="main_image" class="w-50">
                             </div>
                             <div class="input-group">
                                 <div class="custom-file">
@@ -94,20 +94,15 @@
                         <div class="form-group">
                             <label>Тэги</label>
                             <select name="tag_ids[]" class="select2" multiple="multiple" data-placeholder="Выберите тэги" style="width: 100%;">
-                                @foreach($tags as $tag)
-                                    <option
-                                        @if(is_array(old('tag_ids')))
-                                            {{  in_array($tag->id, old('tag_ids')) ? ' selected' : '' }}
-                                        @else
-                                            {{ $postIds = $post->tags->pluck('id') }}
-                                            @foreach($postIds as $postId)
-                                                {{ $tag->id == $postId ? ' selected' : '' }}
-                                            @endforeach
-                                        @endif
-                                    value="{{ $tag->id }}">
-                                        {{ $tag->title }}
-                                    </option>
-                                @endforeach
+                                @if(is_array(old('tag_ids')))
+                                    @foreach($tags as $tag)
+                                        <option {{ in_array($tag->id, old('tag_ids')) ? ' selected' : '' }} value="{{ $tag->id }}"> {{ $tag->title }} </option>
+                                    @endforeach
+                                @else
+                                    @foreach($tags as $tag)
+                                        <option {{ in_array($tag->id, $post->tags->pluck('id')->toArray()) ? ' selected' : '' }} value="{{ $tag->id }}"> {{ $tag->title }} </option>
+                                    @endforeach
+                                @endif
                             </select>
                         </div>
 
