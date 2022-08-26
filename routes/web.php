2a1093eb3155866ab196b1b6fe8ae1c01c1702cd
Yaksat <aubekerov@list.ru>
2022-08-26 11:25:21 +0400

    Добавляем CRUD для пользователей

--- routes/web.php
+++ routes/web.php
@@ -49,6 +49,16 @@ Route::group(['prefix' => 'admin'], function () {
         Route::patch('/{tag}', \App\Http\Controllers\Admin\Tag\UpdateController::class)->name('admin.tag.update');
         Route::delete('/{tag}', \App\Http\Controllers\Admin\Tag\DestroyController::class)->name('admin.tag.destroy');
     });
+
+    Route::group(['prefix' => 'users'], function () {
+        Route::get('/', \App\Http\Controllers\Admin\User\IndexController::class)->name('admin.user.index');
+        Route::get('/create', \App\Http\Controllers\Admin\User\CreateController::class)->name('admin.user.create');
+        Route::post('/', \App\Http\Controllers\Admin\User\StoreController::class)->name('admin.user.store');
+        Route::get('/{user}', \App\Http\Controllers\Admin\User\ShowController::class)->name('admin.user.show');
+        Route::get('/{user}/edit', \App\Http\Controllers\Admin\User\EditController::class)->name('admin.user.edit');
+        Route::patch('/{user}', \App\Http\Controllers\Admin\User\UpdateController::class)->name('admin.user.update');
+        Route::delete('/{user}', \App\Http\Controllers\Admin\User\DestroyController::class)->name('admin.user.destroy');
+    });
 });
 
 Auth::routes();
