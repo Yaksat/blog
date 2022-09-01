1b565052ed8fc616ddf5f48be372c95dafe00b1e
Yaksat <aubekerov@list.ru>
2022-09-01 21:34:22 +0400

    add create users with Job

--- app/Service/PostService.php
+++ app/Service/PostService.php
@@ -16,7 +16,6 @@ class PostService
                 $tagIds = $data['tag_ids'];
                 unset($data['tag_ids']);
             }
-
             $data['preview_image'] = Storage::disk('public')->put('/images', $data['preview_image']);
             $data['main_image'] = Storage::disk('public')->put('/images', $data['main_image']);
 
