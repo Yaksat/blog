6d839c61e11ce8aab1ceb80c41e3dab5b2d06aa4
Damir Aubekerov <80846714+Yaksat@users.noreply.github.com>
2022-09-25 17:57:32 +0400

    show post

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', \App\Http\Controllers\Main\IndexController::class)->name('main.index');
 
+Route::group(['prefix' => 'posts'], function () {
+    Route::get('/', \App\Http\Controllers\Post\IndexController::class)->name('post.index');
+    Route::get('/{post}', \App\Http\Controllers\Post\ShowController::class)->name('post.show');
+});
+
 Route::group(['prefix' => 'personal', 'middleware' => ['auth', 'verified']], function () {
     Route::get('/', \App\Http\Controllers\Personal\Main\IndexController::class)->name('personal.main.index');
 
